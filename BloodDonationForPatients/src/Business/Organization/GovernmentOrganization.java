071aa159f10585e5979d8cb031d39dcce2e2d2a5
Krish Vadhani <vadhani.k@northeastern.edu>
2024-12-02 16:41:30 -0500

    few misc changes

--- BloodDonationForPatients/src/Business/Organization/GovernmentOrganization.java
+++ BloodDonationForPatients/src/Business/Organization/GovernmentOrganization.java
@@ -14,13 +14,11 @@ import java.util.ArrayList;
  */
 public class GovernmentOrganization extends Organization{
 
-    // Constructor to initialize the GovernmentOrganization with a name and parent organization directory
     public GovernmentOrganization(String name, OrganizationDirectory parent) {
         super(name == null ? Organization.Type.Government.getValue() : name, parent);
     }
 
     @Override
-    // Method to return a list of roles supported by the GovernmentOrganization
     public ArrayList<Role> getSupportedRole() {
         ArrayList<Role> roles = new ArrayList();
         roles.add(new GovernmentAgentRole()); // Adding a GovernmentAgentRole to the list
--- BloodDonationForPatients/src/Business/UserAccount/UserAccountDirectory.java
+++ BloodDonationForPatients/src/Business/UserAccount/UserAccountDirectory.java
@@ -6,14 +6,15 @@ package Business.UserAccount;
 
 import Business.Employee.Employee;
 import Business.Role.Role;
-import java.util.ArrayList
+import java.util.ArrayList;
 
 /**
  *
  * @author krish19
  */
 public class UserAccountDirectory {
-       private ArrayList<UserAccount> userAccountList;
+
+    private ArrayList<UserAccount> userAccountList;
 
     public UserAccountDirectory() {
         userAccountList = new ArrayList();  // Initializes the userAccountList
@@ -22,26 +23,28 @@ public class UserAccountDirectory {
     public ArrayList<UserAccount> getUserAccountList() {
         return userAccountList;  // Returns the list of user accounts
     }
-    
-    public boolean checkIfUsernameIsUnique(String username){
+
+    public boolean checkIfUsernameIsUnique(String username) {
         // Checks if the given username is unique in the list
-        for (UserAccount ua : userAccountList){
-            if (ua.getUsername().equals(username))
+        for (UserAccount ua : userAccountList) {
+            if (ua.getUsername().equals(username)) {
                 return false;
+            }
         }
         return true;
     }
-    
-    public UserAccount authenticateUser(String username, String password){
+
+    public UserAccount authenticateUser(String username, String password) {
         // Authenticates the user by matching username and password
-        for (UserAccount ua : userAccountList)
-            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)){
+        for (UserAccount ua : userAccountList) {
+            if (ua.getUsername().equals(username) && ua.getPassword().equals(password)) {
                 return ua;
             }
+        }
         return null;  // Returns null if no matching user is found
     }
-    
-    public UserAccount createUserAccount(String username, String password, Employee employee, Role role){
+
+    public UserAccount createUserAccount(String username, String password, Employee employee, Role role) {
         // Creates a new user account and adds it to the list
         UserAccount userAccount = new UserAccount();
         userAccount.setUsername(username);
